4e9cc388416c9616e3a50c7268c0f9e64bf46a32
Logadeep131 <Logadeep.Ramachandran@wissen.com>
2025-01-20 20:39:22 +0530

    updated with same time start

--- Assignment8/BikeRacing.java
+++ Assignment8/BikeRacing.java
@@ -1,125 +1,120 @@
 import java.util.*;
 
-class Race extends Thread
-{
-	private String name;
-	private int startTime;
-	private int endTime;
+class Race extends Thread {
+    private String name;
+    private int startTime;
+    private int endTime;
     private int totalTime;
-	
 
-	Race(String name)
-	{
-	this.name=name;
-	}
+    static final Object lock = new Object();
 
-	public String getNameRacer() {return this.name;}
+    Race(String name) {
+        this.name = name;
+    }
 
-	public int getStartTime() {return this.startTime;}
+    public String getNameRacer() {
+        return this.name;
+    }
 
-	public int getEndTime() {return this.endTime;}
+    public int getStartTime() {
+        return this.startTime;
+    }
 
-    public int getTotalTime() {return this.totalTime;}
-	
+    public int getEndTime() {
+        return this.endTime;
+    }
 
-	public void run()
-	{
-		this.startTime = (int) System.currentTimeMillis();
+    public int getTotalTime() {
+        return this.totalTime;
+    }
 
-		
-		for(int i=1;i<=20;i++)
-		{
-			try
-			{
-			     int sleepTime=(int)(Math.random()*1000);
-                
-		         Thread.sleep(sleepTime);
-			     if(i==20)
-				 {
-                    this.endTime = (int)System.currentTimeMillis();
-					this.totalTime = this.endTime - this.startTime;
-				 }
-			}
-			
-			catch(Exception e)
-			{
-				System.out.println(e.getMessage());
-			}
-		}
-	}
-	public static void WinnerList(Race[] Racer)
-	{
-		for(int i=0;i<Racer.length-1;i++)
-		{
-			for(int j=i+1;j<Racer.length;j++)
-			{
-			int val1=Racer[i].getTotalTime();
-			int val2=Racer[j].getTotalTime();
-			if(val1>val2)
-			   {
-				Race obj=Racer[i];
-				Racer[i]=Racer[j];
-				Racer[j]=obj;
-			   }
-			}
-		}
-	}
-	public static void display(Race[] Racer)
-	{
-		for(int i=0;i<Racer.length;i++)
-	       {	
-		System.out.print("Rank :"+(i+1)+ " Name : "+Racer[i].getNameRacer());
+    public void run() {
+        try {
+            System.out.println(name + " is ready to race...");
 
-		System.out.println(" Time taken : "+Racer[i].getTotalTime());
-	       }
-	}
+            synchronized (lock) {
+                lock.wait(); 
+            }
 
+            startTime = (int) System.currentTimeMillis();
+
+            for (int i = 1; i <= 10; i++) {
+                try {
+                    int sleepTime = (int) (Math.random() * 100);
+                    Thread.sleep(sleepTime);
+                    if (i == 10) {
+                        endTime = (int) System.currentTimeMillis();
+                        totalTime = endTime - startTime;
+                    }
+                } catch (InterruptedException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void WinnerList(Race[] Racer) {
+        for (int i = 0; i < Racer.length - 1; i++) {
+            for (int j = i + 1; j < Racer.length; j++) {
+                int val1 = Racer[i].getTotalTime();
+                int val2 = Racer[j].getTotalTime();
+                if (val1 > val2) {
+                    Race temp = Racer[i];
+                    Racer[i] = Racer[j];
+                    Racer[j] = temp;
+                }
+            }
+        }
+    }
+
+    public static void display(Race[] Racer) {
+        for (int i = 0; i < Racer.length; i++) {
+            System.out.print("Rank :" + (i + 1) + " Name : " + Racer[i].getNameRacer());
+            System.out.println(" Time taken : " + Racer[i].getTotalTime());
+        }
+    }
 }
 
-class BikeRacing
-{
-	public static void main(String args[])
-	{
+public class BikeRacing {
+    public static void main(String args[]) {
+        Race Racer[] = new Race[10];
 
-	Race Racer[]= new Race[10];
-	
-	for(int i=0;i<10;i++)
-	{
-		Racer[i]=new Race("Racer "+(i+1));
-	}
-
-	for(int i=10;i>=0;i--)
-	{
-		try{
-		System.out.println(i);
-		Thread.sleep(1000);
-		}
-		catch(InterruptedException e)
-		{
-			System.out.println(e.getMessage());
-		}
-	}
-	System.out.println("Go");
-	
-	for(int i=0;i<Racer.length;i++)
-	{ Racer[i].start();
-
-	}
-    for(int i=0;i<Racer.length;i++)
-	{
-        try{
-         Racer[i].join();
+        for (int i = 0; i < Racer.length; i++) {
+            Racer[i] = new Race("Racer " + (i + 1));
         }
-        catch(Exception e)
-        {
+
+        try {
+            System.out.println("Count Down Started....");
+            for (int i = 10; i > 0; i--) {
+                System.out.println(i);
+                Thread.sleep(1000);
+            }
+
+            System.out.println("GO");
+
+            for (Race r : Racer) {
+                r.start();
+            }
+
+            synchronized (Race.lock) {
+                Race.lock.notifyAll(); 
+            }
+
+            for (Race r : Racer) {
+                try {
+                    r.join();
+                } catch (InterruptedException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+
+            Race.WinnerList(Racer);
+            Race.display(Racer);
+
+        } catch (InterruptedException e) {
             System.out.println(e.getMessage());
         }
-
-	}
-	
-	Race.WinnerList(Racer);
-	Race.display(Racer);
-	
-	}
-
+    }
 }
